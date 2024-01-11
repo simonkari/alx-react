0f6fbd68e764e725cecbe5f96e17d8a175a52c98
simonkari <kariithisimon1715@gmail.com>
2024-01-11 19:06:34 +0300

    react components

--- 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import './Notifications.css';
 import PropTypes from 'prop-types';
 
-const NotificationItem = ({ type, html, value }) => {
-	return (
-		<React.Fragment>
-			{type && value ? <li data-notification-type={type}>{value}</li> : null}
-			{html ? (
-				<li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
-			) : null}
-		</React.Fragment>
-	);
+function NotificationItem({ type, html, value }) {
+  return (
+    value ? 
+    <li
+    data-notification-type={type}
+    >{value}</li> 
+    :
+    <li
+    data-notification-type={type}
+    dangerouslySetInnerHTML={html}
+    ></li> 
+  );
+}
+
+NotificationItem.defaultProps = {
+  type: 'default'
 };
 
 NotificationItem.propTypes = {
-	type: PropTypes.string.isRequired,
-	value: PropTypes.string,
-	__html: PropTypes.shape({
-		html: PropTypes.string,
-	}),
-};
-
-NotificationItem.defaultProps = {
-	type: 'default',
+  html: PropTypes.shape({__html: PropTypes.string}),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string
 };
 
 export default NotificationItem;

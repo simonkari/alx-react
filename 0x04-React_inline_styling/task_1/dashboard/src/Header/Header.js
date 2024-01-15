0448e2f9f86f60fec8fa4ac8e2758ce217db7f20
simonkari <kariithisimon1715@gmail.com>
2024-01-15 20:18:07 +0300

    inline styling

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import logo from '../assets/holberton-logo.jpg';
+import logo from '../assets/holberton_logo.jpg';
 import { StyleSheet, css } from 'aphrodite';
 
-const styles = StyleSheet.create({
-	'App-header': {
-		fontSize: '1.4rem',
-		color: 'red',
+function Header() {
+  return (
+    <div className={css(headerStyles.appHeader)}>
+      <img src={logo} alt="logo" className={css(headerStyles.appLogo)}/>
+      <h1 className={css(headerStyles.h1)}>School dashboard</h1>
+    </div>
+  )
+}
+
+const headerStyles = StyleSheet.create({
+	h1: {
+		marginLeft: '3rem'
+	},
+
+	appHeader: {
 		display: 'flex',
+		flexDirection: 'row',
 		alignItems: 'center',
-		padding: '1.2em',
+		color: '#E11D3F',
+		borderBottom: '1px solid #E11D3F',
 	},
 
-	img: {
-		width: '250px',
-		height: '250px',
-	},
+	appLogo: {
+		height: '200px',
+		width: '200px'
+	}
 });
-
-const Header = () => {
-	return (
-		<div className={css(styles['App-header'])}>
-			<img src={logo} alt='Holberton' className={css(styles.img)} />
-			<h1>School dashboard</h1>
-		</div>
-	);
-};
 
 export default Header;
